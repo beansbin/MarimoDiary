d86f108ccb4289891e6ab8455ea33a0aef8a72de
beansbin <beans_bin@naver.com>
2021-08-29 16:10:01 +0900

    Create RegisterVC

---
+++ MarimoDiary/RegisterVC.swift
@@ -0,0 +1,20 @@
+//
+//  RegisterVC.swift
+//  MarimoDiary
+//
+//  Created by 박예빈 on 2021/08/29.
+//
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+

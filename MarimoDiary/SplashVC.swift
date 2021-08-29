f3efa4b5ca69b97f2fe53adca58a6ccc0006c9ee
beansbin <beans_bin@naver.com>
2021-08-29 16:06:32 +0900

    Create SplashView

---
+++ MarimoDiary/SplashVC.swift
@@ -0,0 +1,19 @@
+//
+//  SplashVC.swift
+//  MarimoDiary
+//
+//  Created by 박예빈 on 2021/08/29.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

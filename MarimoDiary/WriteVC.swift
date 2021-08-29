49f868277361462d66e60685b3de5aaa2bf8bb02
beansbin <beans_bin@naver.com>
2021-08-29 16:08:05 +0900

    Create WriteVC

---
+++ MarimoDiary/WriteVC.swift
@@ -0,0 +1,20 @@
+//
+//  WriteVC.swift
+//  MarimoDiary
+//
+//  Created by 박예빈 on 2021/08/29.
+//
+
+import UIKit
+
+class WriteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+

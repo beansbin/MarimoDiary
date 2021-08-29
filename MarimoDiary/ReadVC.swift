ec306246390a2f7650bfcf4af6e8c82c28f43326
beansbin <beans_bin@naver.com>
2021-08-29 16:08:44 +0900

    Create ReadVC

---
+++ MarimoDiary/ReadVC.swift
@@ -0,0 +1,20 @@
+//
+//  ReadVC.swift
+//  MarimoDiary
+//
+//  Created by 박예빈 on 2021/08/29.
+//
+
+import UIKit
+
+class ReadVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+

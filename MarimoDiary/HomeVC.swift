338862ef1b3cfeacbf42751d7a24f8fd59e22741
beansbin <beans_bin@naver.com>
2021-08-29 16:07:20 +0900

    Create HomeView

---
+++ MarimoDiary/HomeVC.swift
@@ -0,0 +1,20 @@
+//
+//  HomeVC.swift
+//  MarimoDiary
+//
+//  Created by 박예빈 on 2021/08/29.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+

c5df3ba06ceebbf978cc36f198dd982ae399815a
beansbin <beans_bin@naver.com>
2021-09-04 18:21:47 +0900

    Add alertAction : to WriteView

--- MarimoDiary/WriteVC.swift
+++ MarimoDiary/WriteVC.swift
@@ -13,6 +13,26 @@ class WriteVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func imageBtn(_ sender: Any) {
+        let alert =  UIAlertController(title: "원하는 타이틀", message: "원하는 메세지", preferredStyle: .actionSheet)
+
+        let library =  UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
+        }
+
+        let camera =  UIAlertAction(title: "카메라", style: .default) { (action) in
+
+        self.openCamera()
+        }
+
+        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+
+        present(alert, animated: true, completion: nil)
+    }
 
     // 화면 터치해서 키보드 내리기
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){

11b04e507fa7b42885b80399792d794677014ec0
beansbin <beans_bin@naver.com>
2021-09-04 20:10:44 +0900

    Modify request permission

--- MarimoDiary/WriteVC.swift
+++ MarimoDiary/WriteVC.swift
@@ -7,13 +7,37 @@
 
 import UIKit
 import PhotosUI
+import AVFoundation
 
 class WriteVC: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
+    
     let picker = UIImagePickerController()
-
+    @IBOutlet weak var imageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         picker.delegate = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // 사진 권한 요청 (최초 요청)
+        PHPhotoLibrary.requestAuthorization { status in
+            return
+        }
+        
+        // 카메라 권한 요청
+        let dialog = UIAlertController(title: "주의", message: "일부 기능이 동작하지 않습니다. [설정] 에서 허용할 수 있습니다.", preferredStyle: .alert)
+        let action = UIAlertAction(title: "확인", style: UIAlertAction.Style.default)
+        dialog.addAction(action)
+        
+        AVCaptureDevice.requestAccess(for: AVMediaType.video) { response in
+            if response {
+                
+            } else {
+                
+            }
+        }
+        
     }
     
     @IBAction func imageBtn(_ sender: Any) {
@@ -39,10 +63,7 @@ class WriteVC: UIViewController, UIImagePickerControllerDelegate & UINavigationC
     }
     
     func openLibrary() {
-        // 사진, 카메라 권한 (최초 요청)
-        PHPhotoLibrary.requestAuthorization { status in
-            return
-        }
+        
         let photoAuthorization = PHPhotoLibrary.authorizationStatus()
         switch photoAuthorization {
         case .authorized:
@@ -50,27 +71,39 @@ class WriteVC: UIViewController, UIImagePickerControllerDelegate & UINavigationC
             present(picker, animated: false, completion: nil)
             break
         default:
-            self.alert("설정에서 사진, 카메라 권한을 허용해 주세요.")
+            self.alert("설정에서 사진 권한을 허용해 주세요.")
         }
        
 
     }
 
     func openCamera(){
-        // 사진, 카메라 권한 (최초 요청)
-        PHPhotoLibrary.requestAuthorization { status in
-            return
-        }
-        let photoAuthorization = PHPhotoLibrary.authorizationStatus()
-        switch photoAuthorization {
+        let cameraAuthorization = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+        print(cameraAuthorization)
+        switch cameraAuthorization {
         case .authorized:
             picker.sourceType = .camera
             present(picker, animated: false, completion: nil)
             break
         default:
-            self.alert("설정에서 사진, 카메라 권한을 허용해 주세요.")
+            self.alert("설정에서 카메라 권한을 허용해 주세요.")
         }
     }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+
+        if let image = info[UIImagePickerController.InfoKey.originalImage.rawValue] as? UIImage{
+
+            imageButton.setImage(image, for: .normal)
+            imageButton.isEnabled = false
+
+                print(info)
+
+            }
+
+            dismiss(animated: true, completion: nil)
+
+        }
 
 
 

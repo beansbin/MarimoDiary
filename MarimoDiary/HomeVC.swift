3c55cb5cdfbeba82f73243b56d0458ac835d8440
beansbin <beans_bin@naver.com>
2021-09-04 16:04:56 +0900

    Set weatherImage

--- MarimoDiary/HomeVC.swift
+++ MarimoDiary/HomeVC.swift
@@ -8,7 +8,7 @@
 import UIKit
 import CoreData
 import CoreLocation
-import Alamofire
+import Foundation
 
 class HomeVC: UIViewController {
     @IBOutlet var writeBtn: UIButton!
@@ -16,6 +16,8 @@ class HomeVC: UIViewController {
     @IBOutlet weak var dDayLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+
+    @IBOutlet weak var weatherImgView: UIImageView!
     
     var locationManager: CLLocationManager? // 위치 관련 이벤트 전달
     var currentLocation: CLLocationCoordinate2D! // 위도, 경도 알려줌
@@ -29,11 +31,7 @@ class HomeVC: UIViewController {
         // 위치 권한 설정
         requestAuthorization()
         
-        // 위도, 경도 출력
-        print(LocationService.shared.longitude)
-        print(LocationService.shared.latitude)
-        
-        WeatherDataManager().setCurrentWeather(lati: Float(LocationService.shared.latitude), longi: Float(LocationService.shared.longitude))
+      //  print(WeatherDataManager().setCurrentWeather(lati: Float(LocationService.shared.latitude), longi: Float(LocationService.shared.longitude), completion: <#()#>))
         
     }
     
@@ -71,6 +69,71 @@ class HomeVC: UIViewController {
             print(error.localizedDescription)
             
         }
+        
+        // 위치 권한 있는지 확인 후 표시
+        var locationAuthorizationStatus : CLAuthorizationStatus
+        let manager = CLLocationManager()
+        
+        if #available(iOS 14.0, *) {
+            locationAuthorizationStatus = manager.authorizationStatus
+            
+        } else {
+            //Fallback on earlier versions
+            locationAuthorizationStatus = CLLocationManager.authorizationStatus()
+        }
+        
+        var weatherImage = UIImage()
+        
+        if locationAuthorizationStatus == .authorizedWhenInUse {
+            var weatherInfo: [String] = []
+            
+            // data fetch
+            WeatherService().getWeather(lati: Float(LocationService.shared.latitude), longi: Float(LocationService.shared.longitude)) { result in
+                switch result {
+                case .success(let weatherResponse):
+                    DispatchQueue.main.async {
+                        weatherInfo.append(weatherResponse.weather.first!.description) // 날씨
+                        weatherInfo.append(String(weatherResponse.main.temp)) // 온도
+                        
+                        let weather = weatherInfo[0]
+                        print(weather)
+                        switch weather {
+                            case "clear sky": fallthrough
+                            case "mist":
+                                weatherImage = UIImage(named: "basic") ?? UIImage(named: "basic")!
+                                break
+                            case "few clouds": fallthrough
+                            case "scattered clouds":
+                                weatherImage = UIImage(named: "cloud") ?? UIImage(named: "basic")!
+                                break
+                            case "broken clouds":
+                                weatherImage = UIImage(named: "cloud") ?? UIImage(named: "basic")!
+                                break
+                            case "shower rain": fallthrough
+                            case "rain": fallthrough
+                            case "thunderstorm":
+                                weatherImage = UIImage(named: "rain") ?? UIImage(named: "basic")!
+                                break
+                            case "snow":
+                                weatherImage = UIImage(named: "snow") ?? UIImage(named: "basic")!
+                            default:
+                                weatherImage = UIImage(named: "basic") ?? UIImage(named: "basic")!
+                        }
+                        self.weatherImgView.image = weatherImage
+                    }
+                case .failure(_ ):
+                    print("error")
+                }
+            }
+            
+           
+        } else { // 없는 경우
+            weatherImage = UIImage(named: "basic") ?? UIImage(named: "basic")!
+        }
+
+       
+        
+        
     }
     
     // 버튼 디자인
@@ -85,49 +148,69 @@ class HomeVC: UIViewController {
         
     }
     
-}
-
-// 날씨값을 반환 하는 데이터 센터
-class WeatherDataManager {
-    static var shread: WeatherDataManager = WeatherDataManager()
-    let baseURL: String = "https://api.openweathermap.org/data/2.5/weather?"
-    let appid: String = "&APPID=2759880296dd0cae8b4b96a8a81eaa37"
-    
-    // apiKey 접근하기
-    private var apiKey: String {
-        get {
-            // 생성한 .plist 파일 경로 불러오기
-            guard let filePath = Bundle.main.path(forResource: "KeyList", ofType: "plist") else {
-                fatalError("Couldn't find file 'KeyList.plist'.")
-            }
-            
-            // .plist를 딕셔너리로 받아오기
-            let plist = NSDictionary(contentsOfFile: filePath)
-            
-            // 딕셔너리에서 값 찾기
-            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else {
-                fatalError("Couldn't find key 'OPENWEATHERMAP_KEY' in 'KeyList.plist'.")
-            }
-            return value
-        }
-    }
 
     
-    func setCurrentWeather(lati: Float, longi: Float)  {
-        let strLati = "lat=\(Float(lati))&"
-        let strLongi = "lon=\(Float(longi))"
-        
-        let url = baseURL + strLati + strLongi + appid
-        print(url)
-        
-        AF.request(url, method: .get, encoding: JSONEncoding.default).validate(statusCode: 200..<300).responseJSON { (json) in
-            let weatherResponse = try! JSONDecoder().decode(WeatherResponse.self, from: json.data!)
-            
-            print(weatherResponse)
-        }
-    }
 }
 
+//// 날씨값을 반환 하는 데이터 센터
+//class WeatherDataManager {
+//    static var shread: WeatherDataManager = WeatherDataManager()
+//    let baseURL: String = "https://api.openweathermap.org/data/2.5/weather?"
+//    let appid: String = "&APPID="
+//
+//    // apiKey 접근하기
+//    private var apiKey: String {
+//        get {
+//            // 생성한 .plist 파일 경로 불러오기
+//            guard let filePath = Bundle.main.path(forResource: "KeyList", ofType: "plist") else {
+//                fatalError("Couldn't find file 'KeyList.plist'.")
+//            }
+//
+//            // .plist를 딕셔너리로 받아오기
+//            let plist = NSDictionary(contentsOfFile: filePath)
+//
+//            // 딕셔너리에서 값 찾기
+//            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else {
+//                fatalError("Couldn't find key 'OPENWEATHERMAP_KEY' in 'KeyList.plist'.")
+//            }
+//            return value
+//        }
+//    }
+//
+//    func setCurrentWeather(lati: Float, longi: Float) -> [String] {
+//        let strLati = "lat=\(Float(lati))&"
+//        let strLongi = "lon=\(Float(longi))"
+//        var resultArray: [String] = []
+//
+//        let url = baseURL + strLati + strLongi + appid + apiKey
+//        print(url)
+//        let semaphore = DispatchSemaphore(value: 0)
+//
+//        do {
+//            AF.request(url, method: .get, encoding: JSONEncoding.default).validate(statusCode: 200..<300).responseJSON { (json) in
+//
+//                let weatherResponse = try! JSONDecoder().decode(WeatherResponse.self, from: json.data!)
+//                resultArray.append(weatherResponse.weather[0].description)
+//                resultArray.append(String(weatherResponse.main.temp))
+//                print(resultArray)
+//                defer {
+//                    semaphore.signal() // 네트워킹 끝나면 신호 보내기
+//                }
+//            }
+//        } catch {
+//            print("날씨 정보를 받아오는 데 실패했습니다.")
+//           // semaphore.signal()
+//        }
+//
+//        semaphore.wait() // 네트워킹 끝날 때 까지 대기
+//
+//
+//        return resultArray
+//    }
+//
+//
+//}
+//
 // 위치 관련 코드
 extension HomeVC : CLLocationManagerDelegate {
     // 위치 권한 요청
@@ -145,32 +228,91 @@ extension HomeVC : CLLocationManagerDelegate {
                locationManager!.startMonitoringSignificantLocationChanges()
            }
        }
-    
+
     // 위도, 경도 알아내기
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         var locationAuthorizationStatus : CLAuthorizationStatus
         if #available(iOS 14.0, *) {
             locationAuthorizationStatus = manager.authorizationStatus
-            
+
         } else {
             //Fallback on earlier versions
             locationAuthorizationStatus = CLLocationManager.authorizationStatus()
         }
-        
+
         if locationAuthorizationStatus == .authorizedWhenInUse {
            currentLocation = locationManager!.location?.coordinate
            LocationService.shared.longitude = currentLocation.longitude
            LocationService.shared.latitude = currentLocation.latitude
         }
     }
-    
+
     class LocationService {
         static var shared = LocationService()
         var longitude:Double!
         var latitude:Double!
     }
+
+
+}
+
+// 에러 정의
+enum NetworkError: Error {
+    case badUrl
+    case noData
+    case decodingError
+}
+
+class WeatherService {
+    // .plist에서 API Key 가져오기
+    private var apiKey: String {
+        get {
+            // 생성한 .plist 파일 경로 불러오기
+            guard let filePath = Bundle.main.path(forResource: "KeyList", ofType: "plist") else {
+                fatalError("Couldn't find file 'KeyList.plist'.")
+            }
+            
+            // .plist를 딕셔너리로 받아오기
+            let plist = NSDictionary(contentsOfFile: filePath)
+            
+            // 딕셔너리에서 값 찾기
+            guard let value = plist?.object(forKey: "OPENWEATHERMAP_KEY") as? String else {
+                fatalError("Couldn't find key 'OPENWEATHERMAP_KEY' in 'KeyList.plist'.")
+            }
+            return value
+        }
+    }
     
     
+    func getWeather(lati: Float, longi: Float, completion: @escaping (Result<WeatherResponse, NetworkError>) -> Void) {
+            let baseURL: String = "https://api.openweathermap.org/data/2.5/weather?"
+            let appid: String = "&APPID="
+            let strLati = "lat=\(Float(lati))&"
+            let strLongi = "lon=\(Float(longi))"
+        
+            let tempUrl = URL(string:  baseURL + strLati + strLongi + appid + apiKey)
+        
+            guard let url = tempUrl else {
+                       return completion(.failure(.badUrl))
+                   }
+        
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let data = data, error == nil else {
+                    return completion(.failure(.noData))
+                }
+                
+                // Data 타입으로 받은 리턴을 디코드
+                let weatherResponse = try? JSONDecoder().decode(WeatherResponse.self, from: data)
+
+                // 성공
+                if let weatherResponse = weatherResponse {
+                    print(weatherResponse)
+                    completion(.success(weatherResponse)) // 성공한 데이터 저장
+                } else {
+                    completion(.failure(.decodingError))
+                }
+            }.resume() // 이 dataTask 시작
+        }
 }
 
 
